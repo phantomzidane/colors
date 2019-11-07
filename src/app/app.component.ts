586046b93327ffdf62c9b64ea01963cee3105dc5
zandroid <30754017+phantomzidane@users.noreply.github.com>
2019-11-07 09:19:30 +0100

    deleted everything to start it over

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import {Colors} from "./data.services";
 
 
 @Component({
@@ -9,142 +8,13 @@ import {Colors} from "./data.services";
 })
 export class AppComponent {
   title = 'color';
-  public data;
-  public colors;
-  public state="color";
-  public url="./assets"
-  public file:File;
-  public cssfile:File;
-  public csss= ``;
-  public page=`
-  <head>
-  <meta charset="utf-8">
-  <title>Color</title>
-  <base href="/">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  
-  <link rel="stylesheet" href="assets/my.css">
-</head>
-  <nav class="navbar">
-  <div class="navbar-left">
-    <a class="navbar-item" href="http://bulma.io">
-      <img src="http://bulma.io/images/bulma-logo.png" alt="Bulma logo">
-    </a>
-  </div>
 
-  <div class="navbar-center">
-    <a class="navbar-item">
-      <span class="icon">
-        <i class="fa fa-github"></i>
-      </span>
-    </a>
-    <a class="nav-item">
-      <span class="icon">
-        <i class="fa fa-twitter"></i>
-      </span>
-    </a>
-  </div>
-
-  <!-- This "nav-toggle" hamburger menu is only visible on mobile -->
-  <!-- You need JavaScript to toggle the "is-active" class on "nav-menu" -->
-  <span class="navbar-toggle">
-    <span></span>
-    <span></span>
-    <span></span>
-  </span>
-
-  <!-- This "nav-menu" is hidden on mobile -->
-  <!-- Add the modifier "is-active" to display it on mobile -->
-  <div class="navbar-right navbar-menu">
-    <a class="navbar-item">
-      Home
-    </a>
-    <a class="navbar-item">
-      Documentation
-    </a>
-    <a class="navbar-item">
-      Blog
-    </a>
-
-    <span class="navbar-item">
-      <a class="button" >
-        <span class="icon">
-          <i class="fa fa-twitter"></i>
-        </span>
-        <span>Tweet</span>
-      </a>
-      <a class="button is-primary">
-        <span class="icon">
-          <i class="fa fa-download"></i>
-        </span>
-        <span>Download</span>
-      </a>
-    </span>
-  </div>
-</nav>`;
-  constructor(private resu:Colors){}
+  constructor(){}
    
   ngOnInit(){
-    this.resu.getColors()
-    .subscribe(resu => this.data = resu );
-    document.querySelector('#ok').innerHTML=this.page;
-  }
-  change(id){
-    this.colors = {
-      "c1":this.data[id].c1,
-      "c2":this.data[id].c2,
-      "c3":this.data[id].c3,
-      "c4":this.data[id].c4,
-    }
-  }
-  test(event){
-    event.target.href="#";
-    if(this.state=="color"){
-      
-      event.target.style.color="red";
-    }
-    else{
-      event.target.href="#";
-      event.target.style.backgroundColor="blue";
-    }
-    event.target.classList.add("how")
-    console.log(event)
-  }
-  col(){
-    if(this.state=="color"){
-      this.state="background-color";
-    }
-    else{
-      this.state="color";
-    }
-  }
-  onFileUpload(event){
-    this.file=event.target.files[0];
-    this.cssfile= event.target.files[1];
-    console.log(this.file)
-    console.log(event)
-  }
-  readTextFile(){
-    // const fd= new FormData();
-    // fd.append("index",this.file, this.file.name)
-    // this.http.post(this.url,fd)
-    // .subscribe(res=>{
-    //   console.log(res)
-    // })
-    let readcss= new FileReader();
-    readcss.onload = (e) =>{
-      this.csss=e.target.result.toString()
-    }
-    readcss.readAsText(this.cssfile);
-    
-    let redfile= new FileReader();
-    redfile.onload = (e) =>{
-      console.log(e.target.result)
-      this.page=e.target.result.toString()
-      document.querySelector('#ok').innerHTML=`<style>${this.csss}</style> ${this.page}`;
-    }
-    redfile.readAsText(this.file);
     
   }
+  
+  
 
 }
--- src/shared/data.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface Data{
-    id:string;
-    c1:string;
-    c2:string;
-    c3:string;
-    c4:string;
-}
